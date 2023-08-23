116b4d902dba0c4acbbc76d45e1387dfd7bf64f9
Namrah-99 <namrahsaeed2@gmail.com>
2023-08-23 10:50:57 +0500

    feat(security): Implemented security best practices

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const globalErrorHandler = require('./controller/errorController');
 const AppError = require('./utils/appError');
@@ -8,12 +13,48 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development Logging
 // console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests for same API
+const limiter = rateLimit({
+  max: 100,
+  windowsMs: 60 * 60 * 1000, // 1hour
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
+// Body parser, reading from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
+
+// Prevent Parameter Pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingAverage',
+      'ratingQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // below mentioned middleware is applied to each single route
@@ -22,6 +63,7 @@ app.use(express.static(`${__dirname}/public`));
 //   next();
 // });
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(x);
